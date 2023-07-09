0e19fb5f3dc6b72e8579b4d834725fb97cf5efb5
superdinoo <serzh.bakalov.81@mail.ru>
2023-07-09 12:21:25 +0400

    firstt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          + Плюс
+        </button>
+      </div>
     </div>
   );
 }
-
 export default App;
